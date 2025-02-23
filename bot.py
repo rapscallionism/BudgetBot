57b3aef1bd3c5e9d8b0248114487a1728c52a65a
Caleb Ninal <marvin.ninal365@gmail.com>
2025-02-23 17:27:56 -0600

    adding more basic commands

--- bot.py
+++ bot.py
@@ -45,5 +45,33 @@ async def calculate(ctx, operation: str, *numbers: float):
 
     await ctx.send(f"The result is: {result}")
 
+@bot.command()
+async def add(context, item_to_add: str, amount: int = 0):
+    """
+        Adds a grocery item to the database
+        To run the command, use `!add_grocery_item <grocery_item> <optional: amount>`
+    """
+
+    if item_to_add == None:
+        await context.send("Please add an item to the grocery list.")
+
+    await add_item_to_grocery_list(context, item_to_add)
+
+# TODO: implement this with database
+async def add_item_to_grocery_list(context, item: str, amount: int = 0):
+    # Database call
+
+    # Return and check
+
+    await context.send(f"TODO: implement this. Adding {amount} numer of {item}(s)")
+
+# Removes the grocery item from the grocery list provided
+@bot.command()
+async def remove(context, item_to_remove: str):
+    if item_to_remove == None:
+        await context.send("Please provide an item to remove.")
+
+    
+
 # Run the bot
 bot.run(BOT_TOKEN)
