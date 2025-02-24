de71b3228fde3ca712e9338c22de3e086c1dd231
Caleb Ninal <marvin.ninal365@gmail.com>
2025-02-24 13:29:51 -0600

    adding test cases; coded up registering

--- testing/bot-test.py
+++ testing/bot-test.py
@@ -16,5 +16,20 @@ BOT_TOKEN = 'MTM0MzM2NjM3NzYyMDI0NjY0MA.G51PC2.nMjZgJLTFKMqma-Q0mu751u5YfX1XLDAx
 async def on_ready():
     print(f"Logged in as {bot.user}")
 
+test_cases: list[str] = [
+    "Register when user does not exist, should be valid",
+    "Register when user does exist, should not be valid",
+    "Add grocery item to list when does not exist, should be valid",
+    "Add grocery item to list when does exist, should be valid and increment",
+    "Add grocery item to list with amount of 1, should be valid",
+    "Add grocery item to list with amount of 0, should not be valid",
+    "Add grocery item to list with amount of 1 and already exists, should be valid and increment",
+    "Remove grocery item from list does exist, should be valid and remove",
+    "Remove grocery item from list does not exist, should not be valid and do nothing",
+    "Remove grocery item from list by increment 1 does exist, should be valid remove 1",
+    "Remove grocery item from list by increment 1 does exist but goes to 0, should be valid and remove entirely from the list",
+    "Remove grocery item from list by increment 1 does not exist, should not be valid and do nothing",
+]
+
 # Run the bot
 bot.run(BOT_TOKEN)
