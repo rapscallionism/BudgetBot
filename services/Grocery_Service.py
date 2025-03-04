f60a654502a9baa2b2051e0278ade55908706763
Caleb Ninal <marvin.ninal365@gmail.com>
2025-03-03 19:53:10 -0600

    adding test.py and adding general impl. of serviec

---
+++ services/Grocery_Service.py
@@ -0,0 +1,20 @@
+
+class Grocery_Service:
+
+    def __init__(self):
+        pass
+
+    def get_all(self):
+        pass
+
+    def get(self, item_to_get: str):
+        pass
+
+    def add(self, item_to_add: str, amount: int = 1):
+        pass
+
+    def remove(self, item_to_remove: str, amount: int = 1):
+        pass
+
+    def remove_all(self, item_to_remove: str):
+        pass
